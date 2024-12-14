268a0180666047f607e544a773c19a969fb26b1e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-15 00:06:13 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import LoginForm from "./components/Login/LoginForm";
+import RegisterForm from "./components/Registration/RegisterForm";
+import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import DepositMoney from "./components/Deposit/DepositMoney";
+import History from "./components/History/TransactionHistory";
+import DetailTransaction from "./components/History/DetailTransactions/DetailTransaction";
+import SendMoney from "./components/SendMoney/SendMoney";
+import Profile from "./components/Profile/Profile";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
+        <Route path="/user" element={<PrivateRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="deposit" element={<DepositMoney />} />
+          <Route path="history" element={<History />} />
+          <Route
+            path="history/:transactionId"
+            element={<DetailTransaction />}
+          />
+          <Route path="send" element={<SendMoney />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
